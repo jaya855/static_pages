d9a570a1e55622904b6bbb239c83cb2ec696eab6
Srivastava <Jaya.Srivastava@motherson.com>
2024-11-09 19:50:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript code to show an alert when the page loads
+// window.onload = function() {
+//     alert('Welcome to My Beautiful Static Website! Enjoy your stay.');
+// }
+console.log("successfully done")
